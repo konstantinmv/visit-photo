0661d0cad3bb86164727329ba0144a666fa3ad19
Konstantin Morozov <konstantin.morozov@rideyego.com>
2020-04-08 16:42:15 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,60 @@
+<script>
+import Nested from './Nested.svelte';
+
+	export let name;
+	name = '<h2>Valera</h2>';
+	let src = '/images/entrance/000040.JPG';
+
+	const handleHello = (event) => {
+    alert(`That is from another component: ${event.detail.text}`);
+	};
+
+</script>
+
+<!-- <main style="background-image: url('images/entrance/000040.JPG')"> -->
+  <div class="imageWelcome" style="background-image: url('images/entrance/000040.JPG')">
+  <p>HELLO</p>
+  <button>
+	Click me
+  </button>
+  </div>
+<!-- </main> -->
+
+<style>
+
+body {
+    margin: 0;
+    text-align: center;
+}
+
+	main {
+		text-align: center;
+		/* padding: 1em;
+		max-width: 240px;
+		margin: 0 auto; */
+	}
+
+	p {
+		color: white;
+		font-size: 2em;
+		align-self: 'center';
+	}
+
+  .imageWelcome {
+    background-size: cover;
+    background-repeat: repeat;
+    height: 95vh;
+    box-sizing: border-box;
+  }
+
+	img {
+		height: 500px;
+		width: 500px;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Nested.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+  let component = "Nested";
+  export let count = 0;
+  let [x, y] = [0, 0];
+  const handleClick = () => {
+      count += 1
+  }
+
+  const wellPrivetik = () => {
+    dispatch('hello', {
+      text: "HELLOUUU!"
+    });
+  }
+
+  const handleMouseMove = (event) => {
+    x = event.clientX;
+    y = event.clientY;
+  }
+
+  $: doubled = count * 2;
+  $: if (count > 30) {
+    count = 5;
+  }
+  const dogs = ['Dzhulka', 'Karamelka', 'Jenny'];
+
+</script>
+
+<p>This is the {component} component</p>
+<button on:click={wellPrivetik}>
+Hello click me
+</button>
+<div on:mousemove|once={handleMouseMove}>
+The mouse position is {x} and {y}
+</div>
